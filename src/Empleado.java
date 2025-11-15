ef9d43cd4275fd1b3fe1ba6688442c2635eaa796
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-15 17:29:09 +0100

    Update Empleado.java
    Introducción de comentarios

--- src/Empleado.java
+++ src/Empleado.java
@@ -2,13 +2,13 @@ public class Empleado {
     private String nombre;
     private double salario;
     private String departamento;
-
+//Creamos el constructor de empleado para que el usuario pueda fijar los datos del empleado
     public Empleado(String nombre, double salario, String departamento) {
         this.nombre = nombre;
         this.salario = salario;
         this.departamento = departamento;
     }
-
+//Creamos los metodos get y setter para que el programa pueda encontrar los datos que se introduzcan
     public String getNombre() {
         return nombre;
     }
@@ -20,7 +20,7 @@ public class Empleado {
     public String getDepartamento() {
         return departamento;
     }
-
+// Creamos el metodo override para que muestre el mensaje en la teminal al  ejercutarlo
     @Override
     public String toString() {
         return "Empleado: " + nombre +
